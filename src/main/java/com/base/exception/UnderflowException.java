bf6d2a82635e303e00f184b73792d457a3cd388b
zhangqw2 <z_q_wei@163.com>
2019-12-04 10:20:34 +0800

    update doc

---
+++ src/main/java/com/base/exception/UnderflowException.java
@@ -0,0 +1,9 @@
+package com.base.exception;
+/**
+ * Exception class for access in empty containers
+ * such as stacks, queues, and priority queues.
+ * @author Mark Allen Weiss
+ */
+public class UnderflowException extends RuntimeException
+{
+}
